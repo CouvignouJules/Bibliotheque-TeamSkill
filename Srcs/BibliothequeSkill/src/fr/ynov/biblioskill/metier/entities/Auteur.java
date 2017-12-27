aa7f20cd3494ceea70b0c08b4dd56d43c55c2cbd
Thomas Alunni-Gradini <thomas.alunni-gradini@ynov.com>
2017-12-27 16:50:54 +0100

    folders arrangement

---
+++ Srcs/BibliothequeSkill/src/fr/ynov/biblioskill/metier/entities/Auteur.java
@@ -0,0 +1,75 @@
+package fr.ynov.biblioskill.metier.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+import fr.ynov.biblioskill.utilitaire.TypeAuteur;
+
+@Entity
+@Table(name ="auteur")
+@NamedQuery(name="lireAuteurs",query="SELECT a FROM Auteur a")
+public class Auteur implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String aProposDe;
+	private String nationalite;
+	private TypeAuteur type;
+
+	public Auteur() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Auteur(Long id, String aProposDe, String nationalite, TypeAuteur type) {
+		super();
+		this.id = id;
+		this.aProposDe = aProposDe;
+		this.nationalite = nationalite;
+		this.type = type;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getaProposDe() {
+		return aProposDe;
+	}
+
+	public void setaProposDe(String aProposDe) {
+		this.aProposDe = aProposDe;
+	}
+
+	public String getNationalite() {
+		return nationalite;
+	}
+
+	public void setNationalite(String nationalite) {
+		this.nationalite = nationalite;
+	}
+
+	public TypeAuteur getType() {
+		return type;
+	}
+
+	public void setType(TypeAuteur type) {
+		this.type = type;
+	}
+	
+	
+
+}
---
+++ Srcs/BibliothequeSkill/src/fr/ynov/biblioskill/service/DaoAuteur.java
@@ -0,0 +1,98 @@
+package fr.ynov.biblioskill.service;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+
+import fr.ynov.biblioskill.metier.entities.Auteur;
+import fr.ynov.biblioskill.service.Dao;
+
+/**
+ * 
+ * @author adrien
+ * Permet de récupérer les données des auteurs en BDD
+ */
+public class DaoAuteur implements Dao<Auteur> {
+
+	@PersistenceContext(unitName="biblioskill")
+	
+	/**
+	 * Utilisé pour la persistence
+	 */
+	private EntityManager em;
+	
+	/**
+	 * ctor
+	 * Initialisation de l'attribut EntityManager
+	 */
+	public DaoAuteur() {
+		em = Persistence.createEntityManagerFactory("biblioskill").createEntityManager();
+	}
+	
+	/**
+	 * Récupère tous les éléments en base
+	 * @return la liste des Auteur
+	 */
+	@Override
+	public List<Auteur> lireTous() {
+		return em.createNamedQuery("lireAuteurs",Auteur.class).getResultList();
+	}
+	
+	/**
+	 * Récupère un seul élément en base en fonction de l'id
+	 * @param cle
+	 * @return le Auteur
+	 */
+	@Override
+	public Auteur lire(int cle) {
+		return em.find(Auteur.class, cle);
+	}
+	
+	/**
+	 * Insère un auteur en base
+	 * @param elt
+	 */
+	@Override
+	public void inserer(Auteur elt) {
+		em.getTransaction().begin();
+		em.persist(elt);
+		em.getTransaction().commit();
+	}
+	
+	/**
+	 * Supprime un auteur en base en fonction de l'id
+	 * @param cle
+	 */
+	@Override
+	public void effacer(int cle) {
+		Auteur tmp = null;
+		em.getTransaction().begin();
+		tmp = em.find(Auteur.class, cle);
+		em.remove(tmp);
+		em.getTransaction().commit();
+	}
+	
+	/**
+	 * Met à jour un auteur en base en fonction de son id
+	 * @param index
+	 * @param obj
+	 */
+	@Override
+	public void update(int index, Auteur obj) {
+		em.getTransaction().begin();
+		update(obj);
+		em.getTransaction().commit();
+	}
+	
+	/**
+	 * Met à jour un auteur en base
+	 * @param obj
+	 */
+	@Override
+	public void update(Auteur obj) {
+		em.merge(obj);
+	}
+
+}
